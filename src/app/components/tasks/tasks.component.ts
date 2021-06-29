8ca23da412a06e68d319bd228a88be087f901bdf
Ana Višnevskaja <anavisnia@gmail.com>
2021-06-29 15:14:52 +0300

    Init

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
+import { Task } from '../../Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+
+  deleteTask(task:Task) {
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id));
+  }
+
+  toggleReminder(task:Task) {
+    // do the opossite of the reminder if true, then makes it false
+    //subscribe is watching for changes and applies them
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
+}
